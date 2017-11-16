828b2181a60b3554bd4a112e6f6400c4540817d4
Roger Stach <rogerdanistach@gmail.com>
2017-11-16 09:43:16 +0700

    Complete rebuild

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 #!/usr/bin/env node
+
 const prompt = require('prompt');
 const request = require('request');
 const cheerio = require('cheerio');
+const print = console.log;
 
 prompt.start()
 
-prompt.get(['full', 'book', 'chapter'], function (err, result) {
-	let url = 'http://alkitab.mobi/tb/' + result.book + '/' + result.chapter
+prompt.get(['book'], function (err, result) {
+	let url = 'http://alkitab.mobi/tb/' + result.book
 	request(url, function(err, resp, body){
 	    if(!err & resp.statusCode == 200) {
 	        const $ = cheerio.load(body);
-	        const total = $('span.style1 strong').eq(2).text();
-		        
-			if (result.full == total) {
-				console.log(result.full + ' does exist.')
-			} else { 
-				console.log(result.full + ' doesn\'t exist.')
-			}
-	    }
+			let chapters = [];
+
+	        $('span.style2 a').each(function (i, elm) {
+				let raw = $(this).text();
+				let firstOne = raw.replace('[', '');
+				let secondOne = firstOne.replace(']', '');
+				let thirdOne = secondOne.replace(' ', '');
+				let fourthOne = '- ' + thirdOne + ' \n';
+
+				chapters.push(fourthOne)
+				// chapters.join(' ')
+				// chapters.replace(',', '')
+				// let final = fifthOne
+	        });
+
+			// if (result.full == total) {
+			// 	console.log(result.full + ' does exist.')
+			// } else {
+			// 	console.log(result.full + ' doesn\'t exist.')
+			// }
+
+			print('\n' + typeof chapters)
+	    } else {
+			print('Error : ' + err)
+		}
 	})
 })
