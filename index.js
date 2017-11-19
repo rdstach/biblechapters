295397e030586e0304f48b420de065a3c7fd4363
Roger Stach <rogerdanistach@gmail.com>
2017-11-19 09:22:11 +0700

    Added chalk package

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 const prompt = require('prompt');
 const request = require('request');
 const cheerio = require('cheerio');
+const chalk = require('chalk'); // TODO: Chalk the text.
 const print = console.log;
 
 prompt.start()
 
 prompt.get(['book'], function (err, result) {
 	let url = 'http://alkitab.mobi/tb/' + result.book
-	request(url, function(err, resp, body){
+	request(url, function(err, resp, body) {
 	    if(!err & resp.statusCode == 200) {
 	        const $ = cheerio.load(body);
 			const chapter = $('span.style1 strong').eq(-1).text()
